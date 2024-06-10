6bf2f09498bb971453808fb86d6e36a1daccac40
whgkswo <gjuk7238@gmail.com>
2024-06-10 18:05:52 +0900

    commit

--- src/main/java/com/springboot/Test.java
+++ src/main/java/com/springboot/Test.java
@@ -1,2 +1,10 @@
-package com.springboot;public class Test {
+package com.springboot;
+
+import java.util.HashMap;
+
+public class Test {
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+        System.out.println(map.remove(1));
+    }
 }
