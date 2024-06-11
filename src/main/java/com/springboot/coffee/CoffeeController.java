2962984497730b95497b85d025fc920b1e2cb0dd
skyla00 <hanulofu@gmail.com>
2024-06-11 10:50:42 +0900

    feature controller

--- src/main/java/com/springboot/coffee/CoffeeController.java
+++ src/main/java/com/springboot/coffee/CoffeeController.java
@@ -1,7 +1,8 @@
 package com.springboot.coffee;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.HashMap;
@@ -26,5 +27,39 @@ public class CoffeeController {
 
     //---------------- 여기서 부터 아래에 코드를 구현하세요! -------------------//
     // 1. 커피 정보 수정을 위한 핸들러 메서드 구현
+    @PatchMapping("/{coffeeId}")
+    // 행위가 일치하는 곳에서 메서드를 실행함.
+    public ResponseEntity patchCoffee (
+            @RequestParam("korName") String korName,
+            @RequestParam("price") String price,
+            @PathVariable long coffeeId ) {
+        // 회원 정보를 수정할 수 있어야 함.
+        // 여기에 추가로 주소를 넣을 수 있음.
+        Map<String, Object> currentCoffee;
+
+        if (coffees.containsKey(coffeeId)) {
+            currentCoffee = coffees.get(coffeeId);
+            currentCoffee.put("korName", korName);
+            currentCoffee.put("price", price);
+            return new ResponseEntity<>(currentCoffee, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
     // 2. 커피 정보 삭제를 위한 핸들러 서드 구현
+    @DeleteMapping("/{coffeeId}")
+    public ResponseEntity deleteMember (
+            @PathVariable String coffeeId ) {
+        // 추가로 회원 정보를 삭제할 수 있어야 함.
+        Map<String, Object> currentCoffee;
+        if(coffees.containsKey(coffeeId)) {
+            coffees.remove(coffeeId);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+    }
+
 }
--- src/main/java/com/springboot/member/MemberController.java
+++ src/main/java/com/springboot/member/MemberController.java
@@ -1,7 +1,8 @@
 package com.springboot.member;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.HashMap;
@@ -16,6 +17,7 @@ public class MemberController {
     public void init() {
         Map<String, Object> member1 = new HashMap<>();
         long memberId = 1L;
+        // key value를 묶은 게 엔트리.
         member1.put("memberId", memberId);
         member1.put("email", "hgd@gmail.com");
         member1.put("name", "홍길동");
@@ -25,7 +27,40 @@ public class MemberController {
     }
 
     //---------------- 여기서 부터 아래에 코드를 구현하세요! --------------------//
-    // 1. 회원 정보 수정을 위한 핸들러 메서드 구현
-    // 2. 회원 정보 삭제를 위한 핸들러 메서드 구현
+    // 1. 회원 정보 수정을 위한 핸들러 메서드 구현 patch
+    @PatchMapping("/{member-id}")
+    // 행위가 일치하는 곳에서 메서드를 실행함.
+    public ResponseEntity patchMember (
+            @RequestParam("phone") String phone,
+            @PathVariable long memberId ) {
+        // 회원 정보를 수정할 수 있어야 함.
+        // 여기에 추가로 주소를 넣을 수 있음.
+        Map<String, Object> currentUser;
+        currentUser = members.get(memberId);
+        // 애초에 null이 들어오는 상황을 피해야 함.
 
+        if(currentUser.containsKey(memberId)) {
+            currentUser.put("phone", phone);
+            return new ResponseEntity<>(currentUser, HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+//        members.get(memberId).put("phone", phone);
+    }
+
+    // 2. 회원 정보 삭제를 위한 핸들러 메서드 구현
+    @DeleteMapping("/{memberId}")
+    public ResponseEntity deleteMember (
+            @PathVariable String memberId ) {
+        // 추가로 회원 정보를 삭제할 수 있어야 함.
+        Map<String, Object> currentUser;
+        if(members.containsKey(memberId)) {
+            members.remove(memberId);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+    }
 }
