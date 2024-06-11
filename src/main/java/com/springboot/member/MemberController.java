cd2915e1e108be3c2afead81c04677fb0a6164de
wooil6 <wooilkf@naver.com>
2024-06-11 10:50:46 +0900

    feature controller

--- src/main/java/com/springboot/member/MemberController.java
+++ src/main/java/com/springboot/member/MemberController.java
@@ -1,7 +1,8 @@
 package com.springboot.member;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.HashMap;
@@ -26,6 +27,31 @@ public class MemberController {
 
     //---------------- 여기서 부터 아래에 코드를 구현하세요! --------------------//
     // 1. 회원 정보 수정을 위한 핸들러 메서드 구현
-    // 2. 회원 정보 삭제를 위한 핸들러 메서드 구현
+    @PatchMapping("/{member-id}")
+    public ResponseEntity patchMemeber(@PathVariable("member-id") long memberId,
+                                       @RequestParam("phone") String phone) {
 
+        if (!members.containsKey(memberId)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        Map<String, Object> currentMember;
+        currentMember = members.get(memberId);
+        currentMember.put("phone", phone);
+//        members.get(memberId).replace("phone",phone);
+//        = new HashMap<>();
+
+        //return new ResponseEntity<>(members.get(memberId), HttpStatus.CREATED);
+        return new ResponseEntity<>(currentMember, HttpStatus.OK);
+    }
+
+
+    // 2. 회원 정보 삭제를 위한 핸들러 메서드 구현
+    @DeleteMapping("/{member-id}")
+    public ResponseEntity removeMember(@PathVariable("member-id") long memberId) {
+        if (!members.containsKey(memberId)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        members.remove(memberId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
