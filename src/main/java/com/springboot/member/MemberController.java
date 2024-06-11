4fe3d142a82283911a9af31efa4499d7dbe882d6
pyyeon <prkyyn@gmail.com>
2024-06-11 10:50:44 +0900

    feature controller

--- src/main/java/com/springboot/member/MemberController.java
+++ src/main/java/com/springboot/member/MemberController.java
@@ -1,9 +1,11 @@
 package com.springboot.member;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
+import java.lang.reflect.Member;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,11 +23,37 @@ public class MemberController {
         member1.put("name", "홍길동");
         member1.put("phone", "010-1234-5678");
 
+
         members.put(memberId, member1);
     }
 
     //---------------- 여기서 부터 아래에 코드를 구현하세요! --------------------//
     // 1. 회원 정보 수정을 위한 핸들러 메서드 구현
+    @PatchMapping(value = "/{member-id}")
+    public ResponseEntity updateMember(@RequestParam("phone") String phone,
+                                        @PathVariable ("member-id") long memberId) {
+        System.out.println("# memberId: " + memberId);
+        if (members.containsKey(memberId)) {
+            members.get(memberId).put("phone", phone);
+        }else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        // not implementation
+        return new ResponseEntity<>(members.get(memberId), HttpStatus.OK);
+    }
+
     // 2. 회원 정보 삭제를 위한 핸들러 메서드 구현
+    @DeleteMapping(value = "/{member-id}") //언더바 못 씀
+    public ResponseEntity deleteMember(@PathVariable("member-id") long memberId) {
+        System.out.println("# remove memberId: " + memberId);
+        if (members.containsKey(memberId)) {
+            members.remove(memberId);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        // not implementation
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
 }
