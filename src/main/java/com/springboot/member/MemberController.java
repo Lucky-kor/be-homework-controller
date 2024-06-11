3a61f4a0a813ecc53e6eb9d31efceebf2126fde2
user <osl123o@naver.com>
2024-06-11 09:31:30 +0900

    mm

--- src/main/java/com/springboot/member/MemberController.java
+++ src/main/java/com/springboot/member/MemberController.java
@@ -17,10 +17,10 @@ public class MemberController {
     public void init() {
         Map<String, Object> member1 = new HashMap<>();
         long memberId = 1L;
-        member1.put("memberId", memberId);
-        member1.put("email", "hgd@gmail.com");
-        member1.put("name", "홍길동");
-        member1.put("phone", "010-1234-5678");
+        member1.put("memberId", memberId); //long
+        member1.put("email", "hgd@gmail.com"); //String
+        member1.put("name", "홍길동"); //String
+        member1.put("phone", "010-1234-5678"); //String
 
         members.put(memberId, member1);
     }
@@ -28,14 +28,23 @@ public class MemberController {
     //---------------- 여기서 부터 아래에 코드를 구현하세요! --------------------//
     // 1. 회원 정보 수정을 위한 핸들러 메서드 구현
     // 2. 회원 정보 삭제를 위한 핸들러 메서드 구현
-    @PatchMapping(path="/{memberId}")
-    public ResponseEntity patchMember(
-            @RequestParam("phone") String phone) {
-        System.out.println("# phone :" + phone);
 
-        // (3) 리턴 값을 ResponseEntity 객체로 변경
-        return new ResponseEntity<>(
+    @PatchMapping(path="/{memberId}")
+    public ResponseEntity patchMember(@PathVariable("memberId") long memberId,
+            @RequestParam("phone") String phone) {
+        members.get(memberId).replace("phone",phone);
+        Map<String,Object> map = members.get(memberId);
+
+        return new ResponseEntity<>(map,
                 HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{memberId}")
+    public ResponseEntity deleteMember(@PathVariable("memberId") long memberId){
+
+        members.remove(memberId);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
